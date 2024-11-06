1d71a05da49095dc818e9e89d0e771f6c3de08c5
bax082024 <bax082024@gmail.com>
2024-11-06 15:01:14 +0100

    changed approach

--- Program.cs
+++ Program.cs
@@ -1,63 +1,24 @@
 ﻿using System;
-using System.Linq;
-using Microsoft.ML;
-using Microsoft.ML.Data;
-using Microsoft.ML.Trainers;
-
 
 class Program 
 {
   static void Main(string[] args)
   {
-    MLContext mLContext = new MLContext();
-
-    // Load Data
-    var data = LoadData(mlContext);
-
-    // Define Pipeline
-    var dataProcessPipeline = mLContext.Transforms.Conversion.ConvertType("Features", nameof(OddEvenData.Number), DataKind.Single);
-
-    var trainer = mLContext.BinaryClassification.Trainers.FastTree(
-      labelColumnName: "Label",
-      featureColumnName: "Features",
-      NumberOfLeaves: 10,
-      learningRate: 0.1,
-      numberOfTrees: 50
-    );
-
-    var trainingPipeline = dataProcessPipeline.Append(trainer);
-
-    // Train the Model
-    var model = trainingPipeline.Fit(data);
-
-    // PredictionEngine for Testing!
-    var predictionEngine = mLContext.Model.CreatePredictionEngine<OddEvenData, OddEvenPrediction>(model);
-
-    // Test predictions
-    var testNumbers = new float[] { 1, 2, 3, 13, 21, 15, 43, 34, 12,};
+    // Test numbers
+    var testNumbers = new int[] { 1, 2, 3, 13, 21, 15, 43, 34, 12 };
+    
     Console.WriteLine("Predictions:");
     foreach (var number in testNumbers)
     {
-      var prediction = predictionEngine.Predict(new OddEvenData { Number = number });
-      Console.WriteLine($"Number: {number}, Prediction: {(prediction.Prediction ? "Even" : "Odd")}, Probability: {prediction.Probability:P2}");
+      // Check if the number is even
+      bool isEven = IsEven(number);
+      Console.WriteLine($"Number: {number}, Prediction: {(isEven ? "Even" : "Odd")}");
     }
   }
 
-}
-
-public class OddEvenData
-{
-  [LoadColumn(0)]
-  public float Number { get; set; }
-
-  [LoadColumn(1)]
-  public bool Label { get; set; }
-}
-
-public class OddEvenPrediction
-{
-  [ColumnName("PredictedLabel")]
-  public bool Prediction { get; set; }
-  public float Probability { get; set; }
-  public float Score { get; set; }
+  static bool IsEven(int number)
+  {
+    // A number is even if it can be divided by 2
+    return number % 2 == 0;
+  }
 }
--- obj/Debug/net8.0/OddEvenClassifier.AssemblyInfo.cs
+++ obj/Debug/net8.0/OddEvenClassifier.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OddEvenClassifier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f018354bb98753246f1a2707832aba81aeb2ce75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63907789d1bafaf798ea7b70c5a49848bfd12157")]
 [assembly: System.Reflection.AssemblyProductAttribute("OddEvenClassifier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OddEvenClassifier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
