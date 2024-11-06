d72faeb819a2819e6339f4db242d9e3e2be6f8f6
bax082024 <bax082024@gmail.com>
2024-11-06 14:25:46 +0100

    added oddevenprediction

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ public class OddEvenData
 {
   [LoadColumn(0)]
   public float Number { get; set; }
-  
+
   [LoadColumn(1)]
   public bool Label { get; set; }
+}
+
+public class OddEvenPrediction
+{
+  [ColumnName("PredictedLabel")]
+  public bool Prediction { get; set; }
+  public float Probability { get; set; }
+  public float Score { get; set; }
 }
